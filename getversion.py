3a66937fc767f5552b36a895d8d7bfa80611a6be
Jewel591 <1106901808@qq.com>
2019-03-02 17:35:06 +0800

    fixes several bugs

--- getversion.py
+++ getversion.py
@@ -2,30 +2,34 @@
 import requests
 import re
 import sys
+import threading
+import time
 # 这两行是为了去除"请求 https 站点取消 ssl 认证时控制台的警告信息"
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-targeturl = sys.argv[1]
-version = {'php': '', 'nginx': '', 'apache': '', 'tomcat': ''}
 
+version = {'php': '', 'nginx': '', 'apache': '', 'tomcat': ''}
+# keyword = {
+#     'php':'PHP Version\s\S\S\S\S\S\S\S',
+#     'tomcat':'Apache Tomcat\S\S\S\S\S\S\S',
+#     'nginx':'nginx\/\S\S\S\S\S*',
+#     'apache':'Apache\/\S*'}
+
+targeturl = ""
 
 class IsUrlOk():
     def refactorurl(self, url):
         global targeturl
-        if not targeturl:
-            print("请输入目标站点...")
-            sys.exit(0)
+        if re.findall("http", url):
+            return
         else:
-            if re.findall("http", url):
+            if re.findall(":443", url):
+                targeturl = "https://" + sys.argv[1]
                 return
             else:
-                if re.findall(":443", url):
-                    targeturl = "https://" + sys.argv[1]
-                    return
-                else:
-                    targeturl = "http://" + sys.argv[1]
-                    return
+                targeturl = "http://" + sys.argv[1]
+                return
 
     def isconnected(self, url):
         global connected
@@ -37,6 +41,12 @@ class IsUrlOk():
             sys.exit(0)
 
     def isurlok(self, url):
+        global targeturl
+        try:
+            targeturl = sys.argv[1]
+        except IndexError:
+            print("请输入目标站点...")
+            sys.exit(0)
         self.refactorurl(url)
         print("正在测试目标站点可访性：", targeturl)
         self.isconnected(targeturl)
@@ -44,9 +54,11 @@ class IsUrlOk():
 
 class GetResponse:
     global targeturl
-
     def request(self, dsturl):
-        return requests.get(dsturl, verify=False, timeout=3, stream=True)
+        try:
+            return requests.get(dsturl, verify=False, timeout=5, stream=True)
+        except IOError:
+            print("连接超时，默认timeout：5")
 
     def getbynormaltext(self):
         return self.request(targeturl).text
@@ -72,27 +84,36 @@ class GetResponse:
 
     def getbyrange(self):
         headers = {'user-agent': 'my-app/0.0.1', 'Range': 'Bytes=1'}
-        return requests.get(targeturl, headers=headers, verify=False).text
+        return requests.get(
+            targeturl,
+            headers=headers,
+            verify=False,
+            timeout=5).text
 
     def getbyPut(self):
-        return requests.put(targeturl, verify=False).text
-
+        try:
+            return requests.put(targeturl, verify=False, timeout=5).text
+        except IOError:
+            print("连接超时，默认timeout：5")
 
 class GetVersion:
 
-    def returnhome(self):
-        return()
-
     def outputphp(self, statement, xversion):
         print(statement, xversion)
-        # sys.exit(0)
-        return self.returnhome()
 
     def refindall(self, keyword, dststr):
-        return re.findall(keyword, str(dststr))
+        try:
+            return (re.findall(keyword, str(dststr)))[0]
+        except IndexError:
+            return re.findall(keyword, str(dststr))
 
     def request(self, dsturl):
-        return requests.get(dsturl, verify=False, timeout=3)
+        try:
+            return requests.get(dsturl, verify=False, timeout=5)
+        except IOError:
+            print("连接超时，默认timeout：5")
+            return ""
+
 
     def getphpversion(self):
         version['php'] = self.refindall(
@@ -102,78 +123,81 @@ class GetVersion:
         else:
             version['php'] = self.refindall(
                 r"PHP\/\S*", GetResponse.getbynormaltext(self))
-            # print(GetResponse.getbynormaltext(self))
             if version['php']:
                 self.outputphp("PHP版本信息（GetByNormal）:", version['php'])
             else:
-                version['php'] = self.refindall(
-                    r"PHP Version\s\S\S\S\S\S\S\S",
-                    GetResponse().getbyphpinfo())
+                version['php']=self.refindall("PHP\/\S*",GetResponse.getby404headers(self))
                 if version['php']:
-                    self.outputphp(
-                        "PHP版本信息（GetByphpinfo.php）:", version['php'])
+                    self.outputphp("PHP版本信息（GetBy404Page）:", version['php'])
                 else:
                     version['php'] = self.refindall(
-                        r"PHP Version\s\S\S\S\S\S\S\S", GetResponse().getbyinfo())
+                        r"PHP Version\s\S\S\S\S\S\S\S",
+                        GetResponse().getbyphpinfo())
                     if version['php']:
                         self.outputphp(
-                            "PHP版本信息（GetByinfo.php）:", version['php'])
+                            "PHP版本信息（GetByphpinfo.php）:", version['php'])
                     else:
-                        # request.get默认只展示302重定向之后的200页面，但是有一种情况 php
-                        # 信息只存在于302页面的 header 字段
                         version['php'] = self.refindall(
-                            r"PHP\/\S\S\S\S\S\S*", requests.head(targeturl).headers)
+                            r"PHP Version\s\S\S\S\S\S\S\S", GetResponse().getbyinfo())
                         if version['php']:
                             self.outputphp(
-                                "PHP版本信息（GetBy302Page）:", version['php'])
+                                "PHP版本信息（GetByinfo.php）:", version['php'])
+                        else:
+                            # request.get默认只展示302重定向之后的200页面，但是有一种情况 php
+                            # 信息只存在于302页面的 header 字段
+                            version['php'] = self.refindall(
+                                r"PHP\/\S\S\S\S\S\S*", requests.head(targeturl).headers)
+                            if version['php']:
+                                self.outputphp(
+                                    "PHP版本信息（GetBy302Page）:", version['php'])
 
     def getapacheversion(self):
-        version['tomcat'] = self.refindall(
+        version['apache'] = self.refindall(
             r"Apache\/\S*", GetResponse.getbynormalheaders(self))
-        if version['tomcat']:
-            self.outputphp("Apache版本信息（GetByNormal）:", version['tomcat'])
+        if version['apache']:
+            self.outputphp("Apache版本信息（GetByNormal）:", version['apache'])
         else:
-            version['tomcat'] = self.refindall(
+            version['apache'] = self.refindall(
                 r"Apache\/\S*", GetResponse.getbynormaltext(self))
-            if version['tomcat']:
-                self.outputphp("Apache版本信息（GetByNormal）:", version['tomcat'])
+            if version['apache']:
+                self.outputphp("Apache版本信息（GetByNormal）:", version['apache'])
             else:
-                version['tomcat'] = self.refindall(
+                version['apache'] = self.refindall(
                     r"Apache\/\S*", GetResponse().getbyphpinfo())
-                if version['tomcat']:
+                if version['apache']:
                     self.outputphp(
-                        "Apache版本信息（GetByphpinfo.php）:", version['tomcat'])
+                        "Apache版本信息（GetByphpinfo.php）:", version['apache'])
                 else:
-                    version['tomcat'] = self.refindall(
+                    version['apache'] = self.refindall(
                         r"Apache\/\S*", GetResponse().getbyinfo())
-                    if version['tomcat']:
+                    if version['apache']:
                         self.outputphp(
-                            "Apache版本信息（GetByinfo.php）:", version['tomcat'])
+                            "Apache版本信息（GetByinfo.php）:", version['apache'])
 
     def getnginxversion(self):
-        version['php'] = self.refindall(
+        version['nginx'] = self.refindall(
             r"nginx\/\S\S\S\S\S*",
             GetResponse.getbynormalheaders(self))
-        if version['php']:
-            self.outputphp("Nginx版本信息（GetByNormal）:", version['php'])
+        if version['nginx']:
+            self.outputphp("Nginx版本信息（GetByNormal）:", version['nginx'])
         else:
-            version['php'] = self.refindall(
+            version['nginx'] = self.refindall(
                 r"nginx\/\S\S\S\S\S*",
                 GetResponse.getbynormaltext(self))
-            if version['php']:
-                self.outputphp("Nginx版本信息（GetByNormal）:", version['php'])
+            if version['nginx']:
+                self.outputphp("Nginx版本信息（GetByNormal）:", version['nginx'])
             else:
-                version['php'] = self.refindall(
+                version['nginx'] = self.refindall(
                     r"nginx\/\S\S\S\S\S*", GetResponse().getbyphpinfo())
-                if version['php']:
+                if version['nginx']:
                     self.outputphp(
-                        "Nginx版本信息（GetByphpinfo.php）:", version['php'])
+                        "Nginx版本信息（GetByphpinfo.php）:", version['nginx'])
                 else:
-                    version['php'] = self.refindall(
+                    version['nginx'] = self.refindall(
                         r"nginx\/\S\S\S\S\S*", GetResponse().getbyinfo())
-                    if version['php']:
+                    if version['nginx']:
                         self.outputphp(
-                            "Nginx版本信息（GetByinfo.php）:", version['php'])
+                            "Nginx版本信息（GetByinfo.php）:", version['nginx'])
 
     def gettomcatversion(self):
         version['tomcat'] = self.refindall(
@@ -210,9 +234,21 @@ class GetVersion:
 
 
 if __name__ == '__main__':
+    timestart = time.time()
     checkurl = IsUrlOk()
     checkurl.isurlok(targeturl)
+    timemiddle = time.time()
     Jewel = GetVersion()
-    Jewel.getallversion()
-    if version['php'] == version['tomcat'] == version['tomcat'] == version['php'] == []:
+    # Jewel.getallversion()
+    for i in (
+            Jewel.getphpversion(),
+            Jewel.gettomcatversion(),
+            Jewel.getnginxversion(),
+            Jewel.getapacheversion()):
+        t = threading.Thread(target=i)
+        t.start()
+    timeend = time.time()
+    if len(version['php']) == len(version['apache']) == len(version['tomcat']) == len(version['nginx']) == 0:
         print("无法探测到任何版本信息，请手工复验")
+    print("scanned in " + str(timeend - timestart) + " seconds")
+    print("检索耗时：", timeend - timemiddle)
